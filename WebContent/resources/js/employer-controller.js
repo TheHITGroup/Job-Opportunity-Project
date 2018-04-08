865dbc59f77829fe237d3e05d1c76f2cbd4f1c6f
ipomajulca <ipomajulca@gmail.com>
2018-04-08 00:06:06 -0400

    adding delete

--- WebContent/resources/js/employer-controller.js
+++ WebContent/resources/js/employer-controller.js
@@ -21,13 +21,34 @@
     angular.module('hitModule').controller('employerController', function ($scope, $rootScope, $location, $http, jobListFactory) {
 
 
-       $scope.jobList = jobListFactory($rootScope.currentUser.id).get(
+        $scope.jobList = jobListFactory($rootScope.currentUser.id).get(
                 function (data) {
                     $scope.jobListSize = data.length;
                 }, function (error) {
             console.log("Error while getting REST data");
         });
+        var user = {
+                    id: $rootScope.currentUser.id
+                }
 
+        $scope.deleteAccount = function () {
+            if (confirm('Are you sure you want to delete your user?')) {
+                console.log("delete");
+                $http({
+                    method: 'POST',
+                    data: user, // object
+                    url: 'ws/user/deleteUser'
+                })
+                        .then(
+                                function (response) {
+                                    console.log("POST success");
+                                    
 
+                                });
+
+            } else {
+                console.log("cancel");
+            }
+        };
     });
 })();
