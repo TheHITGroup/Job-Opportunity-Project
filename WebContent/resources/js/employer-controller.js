a78f315e1922a79403d10cf039ca9eab6674c32a
ipomajulca <ipomajulca@gmail.com>
2018-04-07 23:09:34 -0400

    add list of jobs

--- WebContent/resources/js/employer-controller.js
+++ WebContent/resources/js/employer-controller.js
@@ -1,25 +1,33 @@
 (function () {
-    
-    angular.module('hitModule').factory('jobList',
-        ['$resource', function ($resource) {
-        return function (userId) {
-        var restUrl = "ws/otherRequirements/UJOList";
-                return $resource(restUrl, {}, {
-                get: {
-                method: 'GET',
-                        params: {'userId': userId
-                                },
-                         isArray: true
-                }
 
-                });
-                }
-        }]);
-    
-    
-    
-    angular.module('hitModule').controller('employerController', function ($scope,$rootScope, $location, $http) {
-        
-       //  console.log($rootScope.currentUser.id);
+    angular.module('hitModule').factory('jobListFactory',
+            ['$resource', function ($resource) {
+                    return function (userId) {
+                        var restUrl = "ws/otherRequirements/UJOList";
+                        return $resource(restUrl, {}, {
+                            get: {
+                                method: 'GET',
+                                params: {'userId': userId
+                                },
+                                isArray: true
+                            }
+
+                        });
+                    }
+                }]);
+
+
+
+    angular.module('hitModule').controller('employerController', function ($scope, $rootScope, $location, $http, jobListFactory) {
+
+
+       $scope.jobList = jobListFactory($rootScope.currentUser.id).get(
+                function (data) {
+                    $scope.jobListSize = data.length;
+                }, function (error) {
+            console.log("Error while getting REST data");
+        });
+
+
     });
 })();
